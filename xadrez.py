8b2bc66ee7002dfa23088a0af275c4bb5f23f0cd
Joe-Krth <jp.dosanjosmonteiro@gmail.com>
2025-05-26 21:17:25 -0300

    xadrez.py
    arquivo principal funcionando com todas as alterações feitas nos outros arquivos, com a criação do histórico de jogadas e uma organização melhor do loop que faz o jogo rodar, tá bem mais otimizado agora

--- xadrez.py
+++ xadrez.py
@@ -1,43 +1,43 @@
 from tabuleiro import criar_tabuleiro, exibir
 from pecas import Torre, Cavalo, Bispo, Rainha, Rei, Peao, Peca
 from coordenadas import codigos, coordenadas
-from verificadores import promocao_peao, xeque, xeque_mate, afogamento, validador_peca, validador_movimento
+from verificadores import promocao_peao, xeque, xeque_mate, empates, validador_peca, validador_movimento
 tabuleiro = criar_tabuleiro()
 turno = 1
 
-torre_1 = Torre("branca", (7, 0))
-cavalo_1 = Cavalo("branca", (7, 1))
-bispo_1 = Bispo("branca", (7, 2))
-rainha_1 = Rainha("branca", (7, 3))
-rei_1 = Rei("branca", (7, 4))
-bispo_2 = Bispo("branca", (7, 5))
-cavalo_2 = Cavalo("branca", (7, 6))
-torre_2 = Torre("branca", (7, 7))
-peao_1 = Peao("branca", (6,0))
-peao_2 = Peao("branca", (6,1))
-peao_3 = Peao("branca", (6,2))
-peao_4 = Peao("branca", (6,3))
-peao_5 = Peao("branca", (6,4))
-peao_6 = Peao("branca", (6,5))
-peao_7 = Peao("branca", (6,6))
-peao_8 = Peao("branca", (6,7))
+torre_1 = Torre("torre_1", "branca", (7, 0))
+cavalo_1 = Cavalo("cavalo_1", "branca", (7, 1))
+bispo_1 = Bispo("bispo_1", "branca", (7, 2))
+rainha_1 = Rainha("rainha_1", "branca", (7, 3))
+rei_1 = Rei("rei_1", "branca", (7, 4))
+bispo_2 = Bispo("bispo_2", "branca", (7, 5))
+cavalo_2 = Cavalo("cavalo_2", "branca", (7, 6))
+torre_2 = Torre("torre_2", "branca", (7, 7))
+peao_1 = Peao("peao_1", "branca", (6,0))
+peao_2 = Peao("peao_2", "branca", (6,1))
+peao_3 = Peao("peao_3", "branca", (6,2))
+peao_4 = Peao("peao_4", "branca", (6,3))
+peao_5 = Peao("peao_5", "branca", (6,4))
+peao_6 = Peao("peao_6", "branca", (6,5))
+peao_7 = Peao("peao_7", "branca", (6,6))
+peao_8 = Peao("peao_8", "branca", (6,7))
 
-torre_3 = Torre("preta", (0, 0))
-cavalo_3 = Cavalo("preta", (0, 1))
-bispo_3 = Bispo("preta", (0, 2))
-rainha_2 = Rainha("preta", (0, 3))
-rei_2 = Rei("preta", (0, 4))
-bispo_4 = Bispo("preta", (0, 5))
-cavalo_4 = Cavalo("preta", (0, 6))
-torre_4 = Torre("preta", (0, 7))
-peao_9 = Peao("preta", (1,0))
-peao_10 = Peao("preta", (1,1))
-peao_11 = Peao("preta", (1,2))
-peao_12 = Peao("preta", (1,3))
-peao_13 = Peao("preta", (1,4))
-peao_14 = Peao("preta", (1,5))
-peao_15 = Peao("preta", (1,6))
-peao_16 = Peao("preta", (1,7))
+torre_3 = Torre("torre_3", "preta", (0, 0))
+cavalo_3 = Cavalo("cavalo_3", "preta", (0, 1))
+bispo_3 = Bispo("bispo_3", "preta", (0, 2))
+rainha_2 = Rainha("rainha_2", "preta", (0, 3))
+rei_2 = Rei("rei_2", "preta", (0, 4))
+bispo_4 = Bispo("bispo_4", "preta", (0, 5))
+cavalo_4 = Cavalo("cavalo_4", "preta", (0, 6))
+torre_4 = Torre("torre_4", "preta", (0, 7))
+peao_9 = Peao("peao_9", "preta", (1,0))
+peao_10 = Peao("peao_10", "preta", (1,1))
+peao_11 = Peao("peao_11", "preta", (1,2))
+peao_12 = Peao("peao_12", "preta", (1,3))
+peao_13 = Peao("peao_13", "preta", (1,4))
+peao_14 = Peao("peao_14", "preta", (1,5))
+peao_15 = Peao("peao_15", "preta", (1,6))
+peao_16 = Peao("peao_16", "preta", (1,7))
 
 tabuleiro[7][0] = torre_1
 tabuleiro[7][1] = cavalo_1
@@ -73,6 +73,8 @@ tabuleiro[1][5] = peao_14
 tabuleiro[1][6] = peao_15
 tabuleiro[1][7] = peao_16
 
+historico_jogadas = []
+
 def escolher_peca():
     linha, coluna = coordenadas(input("Peça: "))
     peca = tabuleiro[linha][coluna]
@@ -90,12 +92,15 @@ def validar_peca(peca, linha, coluna):
 def mover_peca(peca, linha, coluna):
     linha_dest, coluna_dest = coordenadas(input("Para: "))
     casa = tabuleiro[linha_dest][coluna_dest]
+    global historico_jogadas
 
     if casa == " ": 
         if (linha_dest, coluna_dest) in validador_movimento(tabuleiro, peca):
             tabuleiro[linha_dest][coluna_dest] = peca
             peca.posicao = (linha_dest, coluna_dest)
             tabuleiro[linha][coluna] = " "
+            m = f"{peca.nome} {peca}{''.join(codigos([(linha_dest, coluna_dest)]))}"
+            historico_jogadas.append(m)
         else:
             print("Movimento Inválido!")
             mover_peca(peca, linha, coluna)
@@ -110,23 +115,27 @@ def mover_peca(peca, linha, coluna):
                 tabuleiro[linha_dest][coluna_dest] = peca
                 peca.posicao = (linha_dest, coluna_dest)
                 tabuleiro[linha][coluna] = " "
+                m = f"{peca.nome} {peca}x{''.join(codigos([(linha_dest, coluna_dest)]))}"
+                historico_jogadas.append(m)
             else:
                 print("Movimento Inválido!")
                 mover_peca(peca, linha, coluna)
-    
+
 while turno != 0:
     promocao_peao(tabuleiro)
     if xeque(tabuleiro):
         if xeque_mate(tabuleiro):
             exibir(tabuleiro)
             break
+        elif empates(tabuleiro, historico_jogadas):
+            exibir(tabuleiro)
+            break
         else:
             print("Xeque!")
     else:
-        if afogamento(tabuleiro):
+        if empates(tabuleiro, historico_jogadas):
             exibir(tabuleiro)
             break
     exibir(tabuleiro)
     escolher_peca()
-
     turno += 1
